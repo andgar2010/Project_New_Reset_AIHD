ed2fd7f7cfe869231b2bded0994eb82557d4a85a
Unknown <afgarcia0479@misena.edu.co>
2017-11-23 16:52:44 -0500

    Add packages-dev

---
+++ WEB/Gruntfile.js
@@ -0,0 +1,34 @@
+//Gruntfile.js
+module.exports = function (grunt) {
+    grunt.initConfig({
+
+        // Watch task config
+        watch: {
+
+        },
+        // Usamos BrowserSync para tus archivos .html, .css, etc, y además le metemos un proxy de nuestro hostname
+        browserSync: {
+            default_options: {
+                bsFiles: {
+                    src: [
+                        "css/*.css",
+                        "js/*.js",
+                        "*.html",
+                        "*.php"
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    proxy: "http://localhost:8080/Project_New_Reset_AIHD/WEB/"
+                }
+            }
+        }
+
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-browser-sync');
+
+    //Tarea por defecto
+    grunt.registerTask('default', ['browserSync' ,'watch']);
+};
