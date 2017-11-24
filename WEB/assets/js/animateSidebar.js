3064d3fc7456f794181f0f8a45f01edfca234039
Unknown <afgarcia0479@misena.edu.co>
2017-11-24 17:29:49 -0500

    Add buttons
    Down - Up (js)

---
+++ WEB/assets/js/animateSidebar.js
@@ -0,0 +1,16 @@
+$('#rotateButton').click(function () {
+    $(this).toggleClass('fa fa-chevron-circle-down');
+    $(this).toggleClass('fa fa-chevron-circle-down fa-fw fa-flip-vertical');
+});
+
+
+$('#rotateButton1').click(function () {
+    $(this).toggleClass('fa fa-chevron-circle-down');
+    $(this).toggleClass('fa fa-chevron-circle-down fa-fw fa-flip-vertical');
+});
+
+
+$('#rotateButton2').click(function () {
+    $(this).toggleClass('fa fa-chevron-circle-down');
+    $(this).toggleClass('fa fa-chevron-circle-down fa-fw fa-flip-vertical');
+});
