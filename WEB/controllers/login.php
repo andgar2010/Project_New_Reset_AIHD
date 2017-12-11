db16208a13c22dd23c8a6d5ac5d427bf47a07252
dayanne stacy agudelo osorio <dsagudelo84@misena.edu.co>
2017-12-11 13:05:52 -0500

    Auto stash before merge of "master" and "origin/master"

--- WEB/controllers/login.php
+++ WEB/controllers/login.php
@@ -1,37 +1,33 @@
 <?php
+
 session_start();
-if (isset($_POST['email'])and isset($_POST['password'])) 
-{
+if (isset($_POST['email'])and isset($_POST['password'])) {
     include 'conexion.php';
-    $emailUsuario             = myqsli_real_escape_string($connection, $_POST['email']);
-    $passwordUsuario          = myqsli_real_escape_string($connection, $_POST['password']);
-    $comprobacionEmail        = 'select * from usuario where correo="'. $emailUsuario.'"';
-    $comprobacion             = $conexion->query($comprobacionEmail);
-    
-    if ($comprobacion->num_rows > 0)
-    {
-        $consulta_bd                   =mysqi_query($conexion, 'select password from usuario where email="' . $emailUsuario.'"');
-        $recoger_dato                  =mysqi_fetch_assoc($consulta_bd);
-        
-        $comprobacion_de_password      = password_verify($passwordUsuario, $recoger_dato['password']);
-        if ($comprobacion_de_password)
-        {
-            $_SESSION['email']  =$emailUsuario;
-            $nombreUsu ='select * from usuario where correo ="' . $emailUsuario . '"';
-            $ejeNomU  = $connection->quey($nombreUsu);
-            $nombre  =mysqi_fetc_assoc($ejeNomU);
-            $_SESSION['name']  = $nombre['nombreUsu'];
-            $_SESSION['ape']   =$nombre['apellidoUsu'];
-            header('location:index.php');
-            
-        }else{
-            header('location:datosInvalidos.php');
+    $emailUsuario = mysqli_real_escape_string($conexion, $_POST['email']);
+    $passwordUsuario = mysqli_real_escape_string($conexion, $_POST['password']);
+    $comprobacionEmail = 'select * from usuario where correo="' . $emailUsuario . '"';
+    $comprobacion = mysqli_query($conexion, $comprobacionEmail);
+
+    if ($comprobacion->num_rows > 0) {
+        $consulta_bd = mysqli_query($conexion, 'select password_us from usuario where correo="' . $emailUsuario . '"');
+        $recoger_dato = mysqli_fetch_assoc($consulta_bd);
+
+        $comprobacion_de_password = password_verify($passwordUsuario, $recoger_dato['password']);
+        if ($comprobacion_de_password) {
+            $_SESSION['email'] = $emailUsuario;
+            $nombreUsu = 'select * from usuario where correo ="' . $emailUsuario . '"';
+            $ejeNomU = $conexion->query($nombreUsu);
+            $nombre = mysqli_fetc_assoc($ejeNomU);
+            $_SESSION['name'] = $nombre['nombreUsu'];
+            $_SESSION['ape'] = $nombre['apellidoUsu'];
+            header('location: dashboard.php');
+        } else {
+            header('location: datosInvalidos.php');
         }
-        
-    }else{
+    } else {
         header('location: sinRegistro.php');
     }
-} else{
-    header('location: login.php');
+} else {
+    header('location: ../index.php');
 }
 ?>
