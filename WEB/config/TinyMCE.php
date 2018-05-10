d04c01d50fc1b3e169c358cec6b9aa08e45919fc
unknown <afgarcia0479@misena.edu.co>
2018-05-10 00:20:19 -0500

    Add TinyMCE PHP

--- WEB/config/TinyMCE.php
+++ WEB/config/TinyMCE.php
@@ -1,10 +1,16 @@
 <?php
 
-require './base_script.php';
+$url_host = '..';
+$js_path        =$url_host .'/assets/js/';
+$framework_path =$url_host .'/assets/Framework/';
+
+$tinyMCE_jquery_js        =$url_host .'/node_modules/tinymce/jquery.tinymce.min.js';
+$tinyMCE_js               =$url_host .'/node_modules/tinymce/tinymce.min.js';
 
 echo '
 <!-- TinyMCE Script  -->
-<script src="' . $TinyMCE_js . '"></script>';
+<script src="' . $tinyMCE_jquery_js . '"></script>
+<script src="' . $tinyMCE_js . '"></script>';
 
 echo "
 <!-- TinyMCE Configure -->
