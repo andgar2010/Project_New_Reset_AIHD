e7c6b1fadd3c45719da2d9758e61cff02eb1ac74
unknown <afgarcia0479@misena.edu.co>
2018-05-12 23:05:46 -0500

    Move add file checkPassword JS

---
+++ WEB/assets/js/checkComparativePassword.js
@@ -0,0 +1,41 @@
+function checkPass() {
+    //Store the Confimation Message Object ...
+    var correcta = false;
+
+    //Set the colors we will be using ...
+    var goodColor = "#66cc66";
+    var badColor = "#ff6666";
+    var styBkColor = "background-color: "
+
+    var pass1 = $("#new_password").val();
+    var pass2 = $("#confirm_password").val();
+
+    //Compare the values in the password field
+    //and the confirmation field
+    if (pass1 == pass2) {
+        //The passwords match.
+        //Set the color to the good color and inform
+        //the user that they have entered the correct password
+        styBkColor += goodColor;
+        $("#new_password").attr("style", styBkColor);
+        $("#confirmMessage")
+            .attr("style", styBkColor)
+            .text("Contraseña correcta!");
+        correcta = true;
+    } else {
+        //The passwords do not match.
+        //Set the color to the bad color and
+        //notify the user.
+        styBkColor += badColor;
+        $("#new_password").attr("style", styBkColor);
+        $("#confirmMessage")
+            .attr("style", styBkColor)
+            .text("Contraseña incorrecta, debe ser igual a la anterior contraseña!");
+    }
+
+    return correcta;
+}
+
+
+
+
