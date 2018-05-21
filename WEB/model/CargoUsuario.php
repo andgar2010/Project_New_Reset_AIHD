25107bce88982a5c8a418bf9e189ddc085d52302
Unknown <afgarcia0479@misena.edu.co>
2018-05-21 12:43:50 -0500

    Add new Class: Area, Cargo, Genero, Rol, TipoDocumento

---
+++ WEB/model/CargoUsuario.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * Modelo Clase Usuario
+ *
+ * @category Class
+ * @package  Model
+ * @author   Andres Garcia <afagrcia0479@misena.edu.co>
+ * @license  <a href="www.mit.org">mit</a>
+ * @version  GIT:<ASD4A6S54DASD>
+ * @link     www.github.com/andgar2010
+ *
+ * This Model of Class User
+ * Source DB
+ */
+
+class CargoUsuario
+{
+    protected $id_cargo;
+    protected $nombre;
+
+    /**
+     * This Construct Class Usuario
+     */
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * Get the value of id_cargo
+     */
+    public function getId_cargo()
+    {
+        return $this->id_cargo;
+    }
+
+    /**
+     * Set the value of id_cargo
+     *
+     * @return  self
+     */
+    public function setId_cargo($id_cargo)
+    {
+        $this->id_cargo = $id_cargo;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of nombre
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * Set the value of nombre
+     *
+     * @return  self
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    /**
+     * Consultar lista de cargo
+     *
+     * @return String arrayListCargo
+     */
+    public function readCargo()
+    {
+        include '../config/Database.php';
+        $sql_query = "SELECT * FROM cargo_usuario";
+
+        if ($output_sql = $db->query($sql_query)) {
+
+            if ($row = $output_sql->num_rows == 0) {
+                echo'No hay datos de Cargo Usuarios';
+            } else {
+                while ($row = $output_sql->fetch_assoc()) {
+                    echo'<option value="'.$row['id_cargo'].'">'.$row['nombre'].'</option>';
+                }
+                $output_sql->close();
+            }
+            $db->close();
+        }
+
+    }//End readCargo()
+}
+?>
---
+++ WEB/model/GeneroUsuario.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ * Modelo Clase Usuario
+ *
+ * @category Class
+ * @package  Model
+ * @author   Andres Garcia <afagrcia0479@misena.edu.co>
+ * @license  <a href="www.mit.org">mit</a>
+ * @version  GIT:<ASD4A6S54DASD>
+ * @link     www.github.com/andgar2010
+ *
+ * This Model of Class User
+ * Source DB
+ */
+
+class GeneroUsuario
+{
+    protected $id_genero;
+    protected $nombre;
+
+    /**
+     * This Construct Class Usuario
+     */
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * Get the value of id_genero
+     */
+    public function getId_genero()
+    {
+        return $this->id_genero;
+    }
+
+    /**
+     * Set the value of id_genero
+     *
+     * @return  self
+     */
+    public function setId_genero($id_genero)
+    {
+        $this->id_genero = $id_genero;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of nombre
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * Set the value of nombre
+     *
+     * @return  self
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    /**
+     * Consultar lista de genero
+     *
+     * @return String arrayListGenero
+     */
+    public function readGenero()
+    {
+        include '../config/Database.php';
+        $sql_query = "SELECT * FROM genero_usuario";
+
+        if ($output_sql = $db->query($sql_query)) {
+
+            if ($row = $output_sql->num_rows == 0) {
+                echo'No hay datos de Tipo de Documento Usuarios';
+            } else {
+                while ($row = $output_sql->fetch_assoc()) {
+                    echo'
+                    <label
+                        id="cod_genero"
+                        for="'.$row['nombre'].'"
+                        class="radio-inline pmd-radio pmd-radio-ripple-effect"
+                    >
+                    <input
+                        type="radio"
+                        name="cod_genero"
+                        id="'.$row['nombre'].'"
+                        value="'.$row['id_genero'].'"
+                        required
+                    >
+                        &nbsp;'.$row['nombre'].'
+                    </label>';
+                }
+                $output_sql->close();
+            }
+            $db->close();
+        }
+
+    }//End readGenero()
+}
+?>
