a7cbb536681f8560d493a9ae6894788a33133bcf
Unknown <afgarcia0479@misena.edu.co>
2018-05-25 17:57:55 -0500

    Saved Classroom

--- WEB/test/TestingNewReset/Scripts/Ingreso Usuario SuperAdministrador/Script1527288956300.groovy
+++ WEB/test/TestingNewReset/Scripts/Ingreso Usuario SuperAdministrador/Script1527288956300.groovy
@@ -18,35 +18,4 @@ import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUiBuiltInKeywords
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
-import org.openqa.selenium.Keys as Keys
-
-WebUI.openBrowser('')
-
-WebUI.navigateToUrl('http://localhost:3000/Project_New_Reset_AIHD/WEB/views/dashboard.php')
-
-WebUI.click(findTestObject('Page_New Reset AIHD Admin Dashboard/span_Gestin de usuarios'))
-
-WebUI.click(findTestObject('Page_New Reset AIHD Admin Dashboard/button_Agregar usuario'))
-
-WebUI.click(findTestObject('Page_Registro nuevo usuario/label_CC'))
-
-WebUI.setText(findTestObject('Page_Registro nuevo usuario/input_num_cedula'), '379164285')
-
-WebUI.setText(findTestObject('Page_Registro nuevo usuario/input_nombre'), 'Felipe')
-
-WebUI.setText(findTestObject('Page_Registro nuevo usuario/input_apellido'), 'Ramirez')
-
-WebUI.setText(findTestObject('Page_Registro nuevo usuario/input_email'), 'fr@gmail.com')
-
-WebUI.click(findTestObject('Page_Registro nuevo usuario/label_Masculino'))
-
-WebUI.selectOptionByValue(findTestObject('Page_Registro nuevo usuario/select_------ Seleccionar ----'), '4', true)
-
-WebUI.selectOptionByValue(findTestObject('Page_Registro nuevo usuario/select_------ Seleccionar ----_1'), '3', true)
-
-WebUI.selectOptionByValue(findTestObject('Page_Registro nuevo usuario/select_------ Seleccionar ----_2'), '3', true)
-
-WebUI.click(findTestObject('Page_Registro nuevo usuario/button_Crear nuevo usuario'))
-
-WebUI.closeBrowser()
 
