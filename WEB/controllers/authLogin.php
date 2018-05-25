a6e01292b2576118d9153f764a38e2ffcf35adc2
Unknown <afgarcia0479@misena.edu.co>
2018-05-24 22:15:42 -0500

    Update authLogin.php

--- WEB/controllers/authLogin.php
+++ WEB/controllers/authLogin.php
@@ -1,176 +1,92 @@
 <?php
 // ob_start();
-// session_start();
+session_start();
 
 require '../config/Database.php';
 require '../model/Usuario.php';
 $usuario = new Usuario();
-    if (isset($_POST['email']) && isset($_POST['password'])) {
-        echo '<pre>';
-        var_dump($_POST);
-        echo '</pre>';
 
-        $usuario->setEmail($_POST['email']);
-        $usuario->setPassword($_POST['password']);
+if (isset($_POST['email']) && isset($_POST['password'])) {
 
-        $email      = $usuario->getEmail();
-        $password   = $usuario->getPassword();
-        echo 'Ingresado usuario de email: '. $usuario->getEmail();
-        echo '<br>Ingresado usuario de password: '. $usuario->getPassword() . "<br>";
+    // echo '<pre>';
+    // var_dump($_POST);
+    // echo '</pre>';
 
-        $query_sql = "SELECT * FROM usuario WHERE email = '". $email ."' AND password='" . $password."'";
-        echo $query_sql;
+    $usuario->setEmail($_POST['email']);
+    $usuario->setPassword($_POST['password']);
 
-        $output_sql = $db->query($query_sql);
+    $email      = $usuario->getEmail();
+    $password   = $usuario->getPassword();
 
-        if ($row = $output_sql->num_rows == 0) {
+    // echo 'Ingresado usuario de email: '. $usuario->getEmail();
+    // echo '<br>Ingresado usuario de password: '. $usuario->getPassword() . "<br>";
 
-            header('location: ../index.php?info=noFoundDB');
+    $query_sql = "SELECT * FROM usuario WHERE email = '". $email ."' AND password='" . $password."'";
 
-        } else {
+    $output_sql = $db->query($query_sql);
 
-            while ($obj = $output_sql->fetch_object()) {
+    if ($row = $output_sql->num_rows == 0) {
 
-                // echo '<pre>';
-                // var_dump($obj);
-                // echo '</pre>';
+        header('location: ../index.php?info=noFoundDB');
 
-                // echo '<br>Ingresado usuario de email: '. $email;
-                // echo '<br>Ingresado usuario de password: '. $password;
-                // echo '<br>Se encontrado en BD';
-                // echo '<br>BD usuario de email: '. $obj->email;
-                // echo '<br>BD usuario de password: '. $obj->password;
+    } else {
 
-                $usuario->setId_usuario($obj->id_usuario);
-                $usuario->setCod_tipo_doc($obj->cod_tipo_doc);
-                $usuario->setDocumento($obj->documento);
-                $usuario->setNombre($obj->nombre);
-                $usuario->setApellido($obj->apellido);
-                $usuario->setCod_genero($obj->cod_genero);
-                $usuario->setEmail($obj->email);
-                $usuario->setCod_area($obj->cod_area);
-                $usuario->setCod_cargo($obj->cod_cargo);
-                $usuario->setCod_rol($obj->cod_rol);
-                $usuario->setCod_estado_usuario($obj->cod_estado_usuario);
-            }
-
-            $_SESSION['nombre']     = $usuario->getNombre;
-            $_SESSION['apellido']   = $usuario->getApellido;
-            $_SESSION['cod_rol']    = $usuario->getCod_rol;
+        while ($obj = $output_sql->fetch_object()) {
+            $usuario->setId_usuario($obj->id_usuario);
+            $usuario->setCod_tipo_doc($obj->cod_tipo_doc);
+            $usuario->setDocumento($obj->documento);
+            $usuario->setNombre($obj->nombre);
+            $usuario->setApellido($obj->apellido);
+            $usuario->setCod_genero($obj->cod_genero);
+            $usuario->setEmail($obj->email);
+            $usuario->setCod_area($obj->cod_area);
+            $usuario->setCod_cargo($obj->cod_cargo);
+            $usuario->setCod_rol($obj->cod_rol);
+            $usuario->setCod_estado_usuario($obj->cod_estado_usuario);
+        }
 
         $output_sql->close();
-        header('location:../views/dashboard.php');
+
     }
     $db->close();
-
 }
 
+$nombresCompletos = $usuario->getNombre().' '.$usuario->getApellido();
 
+$_SESSION['nombre']                 = $usuario->getNombre();
+$_SESSION['apellido']               = $usuario->getApellido();
+$_SESSION['cod_rol']                = $usuario->getCod_rol();
+$_SESSION['cod_estado_usuario']     = $usuario->getCod_estado_usuario();
+$_SESSION['nombres_completos']      = $nombresCompletos;
 
+switch ($_SESSION['cod_estado_usuario']) {
+case '1': // -- cod_estado_usuario 1 Inactivo --
+    header('location: ../index.php?info=stuInactive');
+    break;
+// -- End cod_estado_usuario 1 Inactivo --
 
+case '2': // -- cod_estado_usuario 2 Activo --
 
+    switch ($_SESSION['cod_rol']) {
+    case '1'://SuperAdministrador
+        header('location:../views/dashboard.php');
+        break;
+    case '2'://Tecnico
+        header('location:../views/viewListUsers.php');
+        break;
+    case '3'://Administrativo
+        header('location:../views/viewAddUser.php');
+        break;
+    case '4'://Profesor
+        header('location:../views/listTicket.php');
+        break;
+    }//End Switch cod_rol
 
+    break;
+// -- End cod_estado_usuario 2 Activo --
 
-// echo '<pre>';
-// var_dump($db->query($sql_query));
-// echo '</pre>';
-
-
-
-// if (isset($_POST['email']) && isset($_POST['password'])) {
-
-
-
-//  else {
-
-    //     while ($obj = $output_sql->fetch_object()) {
-
-    //         echo 'Ingresado usuario de email: '. $email;
-    //         echo '<br>Ingresado usuario de password: '. $password;
-    //         echo '<br>Se encontrado en BD';
-    //         echo 'BD usuario de email: '. $obj->email;
-    //         echo '<br>BD usuario de password: '. $obj->password;
-
-    //         // $usuario->id_usuario   = $db->sanitize($obj->id_usuario);
-    //         // $usuario->cod_tipo_doc = $db->sanitize($obj->cod_tipo_doc);
-    //         // $usuario->documento    = $db->sanitize($obj->documento);
-    //         // $usuario->nombre       = $db->sanitize($obj->nombre);
-    //         // $usuario->apellido     = $db->sanitize($obj->apellido);
-    //         // $usuario->cod_genero   = $db->sanitize($obj->cod_genero);
-    //         // $usuario->email        = $db->sanitize($obj->email);
-    //         // $usuario->cod_area     = $db->sanitize($obj->cod_area);
-    //         // $usuario->cod_cargo    = $db->sanitize($obj->cod_cargo);
-    //         // $usuario->cod_rol      = $db->sanitize($obj->cod_rol);
-    //         // $usuario->cod_estado   = $db->sanitize($obj->cod_estado);
-    //     }
-
-    //     $output_sql->close();
-    // }
-    // $db->close();
-
-// }
-
-
-// if ( isset($_POST['email']) && isset($_POST['password']) ) {
-//     $email      = $usuario->sanitize($_POST['email']);
-//     $password   = $usuario->sanitize($_POST['password']);
-
-//     $query_sql = "SELECT * FROM usuario WHERE email = '$email'";
-
-//     $output_sql = $db->query($sql_query);
-
-//     if ($row = $output_sql->num_rows == 0) {
-//         header('location: ../index.php?info=noFoundDB');
-//     } else {
-
-//         while ($obj = $output_sql->fetch_object()) {
-//             $usuario->id_usuario   = $db->sanitize($obj->id_usuario);
-//             $usuario->cod_tipo_doc = $db->sanitize($obj->cod_tipo_doc);
-//             $usuario->documento    = $db->sanitize($obj->documento);
-//             $usuario->nombre       = $db->sanitize($obj->nombre);
-//             $usuario->apellido     = $db->sanitize($obj->apellido);
-//             $usuario->cod_genero   = $db->sanitize($obj->cod_genero);
-//             $usuario->email        = $db->sanitize($obj->email);
-//             $usuario->cod_area     = $db->sanitize($obj->cod_area);
-//             $usuario->cod_cargo    = $db->sanitize($obj->cod_cargo);
-//             $usuario->cod_rol      = $db->sanitize($obj->cod_rol);
-//             $usuario->cod_estado   = $db->sanitize($obj->cod_estado);
-//         }
-
-//         $output_sql->close();
-//     }
-//     $db->close();
-
-//     echo 'Ingresado usuario de email: '. $email;
-//     echo '<br>Ingresado usuario de password: '. $password;
-//     echo '<br> dato de BD usuario email: '. $usuario->password;
-//     echo '<br> dato de BD usuario password: '. $usuario->email;
-
-
-//     // // Si encuentra igual registro de email y password de BD. y ingresado usuario && ($password == $passwordDB)
-//     // if (($email == $usuario->email) && ($password == $usuario->password))  {
-//     //     $_SESSION['nombre']     = $usuario->nombre;
-//     //     $_SESSION['apellido']   = $usuario->apellido;
-//     //     $_SESSION['cod_rol']    = $usuario->cod_rol;
-
-//     //     header('location:../views/dashboard.php');
-
-//     // } else {
-//     //     header('location:../index.php?info=errorUserOrPass');
-//     // }
-
-// } else {
-//     header('location:../index.php?info=noInput');
-// }
-
-// if (isset($_POST['email']))
-// {
-//     include './conexion.php';
-
-//     $Email = mysqli_real_escape_string($conexion, $_POST['email']);
-//     $sqlQuery = 'SELECT * FROM usuario WHERE = "' .$Email. "'";
-//     $EjecutarQuery = $conexion->query($sqlQuery) or die('Hubo un error al consultar al correo ' . mysqli_error($conexion));
-//     mysqli_close($conexion);
-//     header('location: ../index.php');
-// }
+default:
+    # code...
+    break;
+}
 ?>
