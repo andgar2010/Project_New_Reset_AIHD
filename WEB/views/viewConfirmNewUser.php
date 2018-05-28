3a8fc7b0bbfa3a79e3bbdd4801deab18374ff7c2
Unknown <afgarcia0479@misena.edu.co>
2018-05-28 00:51:32 -0500

    Add new view Confirm new User

---
+++ WEB/views/viewConfirmNewUser.php
@@ -0,0 +1,172 @@
+<?php
+/**
+ * Modelo Clase Usuario
+ *
+ * @category View
+ * @package  GestionUsuarios
+ * @author   Andres Garcia <afagrcia0479@misena.edu.co>
+ * @license  <a href="www.mit.org">mit</a>
+ * @version  GIT:<ASD4A6S54DASD>
+ * @link     www.github.com/andgar2010
+ *
+ * Editar cuenta de usuario MVC Source DB
+ */
+
+if (isset($_GET['id'])) {
+
+    $id_usuario = intval($_GET['id']);
+    include '../model/Usuario.php';
+    $usuario = new Usuario();
+    $usuario->readSingleRecordUsuer($id_usuario);
+
+    if ($usuario->getNombre() == null) {
+
+        //Si no encontrado registro de usuario en base de datos, redireccionar -> index.php con parametros
+        header("location: ../index.php?info=noFoundDBNewUser");
+
+    } else {
+
+        //Si encontrado registro de usuario en base de datos, llevar datos de usuario al formulario
+        $id_usuario             = $usuario->getId_usuario();
+        $cod_tipo_doc           = $usuario->getCod_tipo_doc();
+        $documento              = $usuario->getDocumento();
+        $nombre                 = $usuario->getNombre();
+        $apellido               = $usuario->getApellido();
+        $cod_genero             = $usuario->getCod_genero();
+        $email                  = $usuario->getEmail();
+        $temp_password          = $usuario->getPassword();
+        $cod_area               = $usuario->getCod_area();
+        $cod_cargo              = $usuario->getCod_cargo();
+        $cod_rol                = $usuario->getCod_rol();
+        $cod_estado_usuario     = $usuario->getCod_estado_usuario();
+    }
+
+} else {
+
+    header('location: ../index.php?info=noFoundDBNewUser');
+}
+?>
+
+    <!DOCTYPE html>
+    <html lang="es">
+
+    <head>
+        <!-- HTML Meta Tags -->
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Confirmar nuevo del usuario</title>
+        <meta name="description" content="Confirmar nuevo usuario de NEW RESET A.H.I.D.">
+        <!-- End HTML Meta Tags -->
+
+        <?php   require '../config/base_head.php';
+            //require '../config/googleAnaytics.php';?>
+
+    </head>
+
+    <body>
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="section-title modal-title text-center">Confirmar nuevo usuario</h1>
+                </div>
+                <hr>
+                <div class="modal-body">
+            <?php
+                echo'
+                <form method="post" action="../controllers/controllerConfirmNewUser.php" class="">
+
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <div class="form-group">
+                                <input type="hidden" name="id_usuario" id="id_usuario" class="form-control" value="'.$id_usuario.'">
+                                <input type="hidden" name="temp_password_db" id="temp_password_db" class="form-control" value="'.$temp_password.'">
+                                <input type="hidden" id="documento" name="documento" value="'.$documento.'" class="form-control">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <div class="form-group">
+                                <label for="nombre" class="control-label">Nombre de usuario</label>
+                                <input id="nombre" type="text" name="nombre" class="form-control" value="'.$nombre.'" title="Por favor ingresa correctamente nombre del usuario"
+                                    required/>
+                            </div>
+                        </div>
+
+                        <div class="col-sm-6">
+                            <div class="form-group">
+                                <label for="apellido" class="control-label">Apellido de usuario</label>
+                                <input id="apellido" type="text" name="apellido" class="form-control" value="'.$apellido.'" title="Por favor ingresa correctamente apellido del usuario"
+                                    required/>
+                            </div>
+                        </div>
+                    </div>';
+            ?>
+
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <div class="form-group">
+                                <div class="row">
+                                    <div class="col-sm-5"></div>
+                                    <div class="col-sm-5">
+                                        <label for="cod_genero" class="control-label text-center">Contraseña</label>
+                                    </div>
+                                </div>
+                            <div class="row">
+                                    <div class="col-sm-2"></div>
+                                    <div class="col-sm-8">
+                                        <div class="form-group">
+                                            <input id="password" type="password" name="temp_password_input" placeholder="Temporal contraseña por enviado a su correo" class="form-control" />
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        <div class="form-group">
+                                            <input id="new_password" type="password" name="new_password" placeholder="Nueva contraseña" class="form-control" title="La contraseña debe tener minimo 7 caracteres, una letra mayúscula, una letra minúscula, un numero y un simbolo."  pattern="[a-zA-Z0-9]{7,30}"/>
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <div class="form-group">
+                                            <input id="confirm_password" type="password" name="new_password_repeat" placeholder="Nueva contraseña (repetir)" class="form-control" required onkeyup="checkPass(); return false;" />
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-sm-10">
+                                        <span id="confirmMessage"></span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <hr>
+
+                    <div class="row">
+                        <div class="col-lg-5 col-md-5 col-sm-5 col-xs-12 pull-right">
+                            <button id="enviar" name="btnClickedUser" value="sendCreated" type="submit" class="btn btn-block btn-success pmd-z-depth-3 pmd-ripple-effect" onsubmit="return checkPass()">Confirmar</button>
+                        </div>
+                        &nbsp;
+                        <div class="col-lg-5 col-md-5 col-sm-5 col-xs-12 pull-right">
+                            <a id="listUsers" href="../index.php">
+                                <button id="cancelar" name="cancelar" type="button" class="btn btn-block btn-danger pmd-z-depth-3 pmd-ripple-effect">Cancelar</button>
+                            </a>
+                        </div>
+
+                    </div>
+                    <br>
+                </form>
+            </div>
+        </div>
+    </div>
+    <div class="modal-footer"></div>
+
+    <?php require '../config/base_script.php';?>
+
+    <script src="../assets/js/checkComparativePassword.js"></script>
+
+</body>
+</html>
