8c33b47e9594ba49eeac181e2c3ef30534af91cd
Unknown <afgarcia0479@misena.edu.co>
2018-06-24 23:06:50 -0500

    Add Test File PHPMailer

--- WEB/old/testmail/index.php
+++ WEB/old/testmail/index.php
@@ -1,80 +1,76 @@
 <!doctype html>
-<html lang="en">
+<html lang="es">
 
 <head>
-    <title>Title</title>
+    <title>PHPMailer Enviar desde Localhost</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+        crossorigin="anonymous">
 </head>
 
 <body>
 
-<div class="container">
-<div class="row">
-<div class="content">
-        <div class="contact-main">
-            <form action="" method="post">
-                <h3>Tu correo electronico</h3>
-                <input type="email" name="customer_email" id="email" class="form-control" required>
-                <label for="" class="control-label">Nombre</label>
-                <input type="text" name="customer_name" id="name" class="form-control" required>
-                <label for="" class="control-label">Subject</label>
-                <input type="text" name="subject" id="subject" class="form-control" required>
-                <br>
-                <h3 class="control-label">Mensaje</h3>
-                <textarea name="message" id="message" placeholder="Leave your message here..." required></textarea>
-                <?php
-                    if (isset($_POST['send'])) {
-                        include('sendemail.php'); //Mando a llamar ala funcion que se encarga de enviar el correo eletronico
+    <div class="container">
+        <div class="row">
+            <div class="content">
+                <div class="contact-main">
+                    <form action="" method="post">
+                        <h3>Correo electronico para enviar al quien</h3>
+                        <input type="email" name="customer_email" id="email" class="form-control" required>
+                        <label for="" class="control-label">Nombre</label>
+                        <input type="text" name="customer_name" id="name" class="form-control" required>
+                        <label for="" class="control-label">Subject</label>
+                        <input type="text" name="subject" id="subject" class="form-control" required>
+                        <br>
+                        <h3 class="control-label">Mensaje</h3>
+                        <textarea name="message" id="message" placeholder="Leave your message here..." required></textarea>
+                        <?php
+                        if (isset($_POST['send'])) {
 
-                        /* Configuracion de variables para enviar el correo */
-                        $mail_username              = 'soportenewreset@gmail.com'; //Correo electronico saliente. ejemplo: tucorreo@gmail.com
-                        $mail_userpassword          = 'AG40DA48CT41'; //Tu contraseña correo
-                        $mail_sendtoAddAddressMail  = '';
-                        $template         = "email_template.html"; //Ruta de la plantilla HTML para enviar nuestro mensaje
+                            //Mando a llamar a la funcion que se encarga de enviar el correo eletronico
+                            include('sendemail.php');
 
-                        /* Inicio captura de datos enviados por $_POST para enviar el correo */
-                        $mail_setFromEmail = $_POST['customer_email'];
-                        $mail_setFromName = $_POST['customer_name'];
-                        $txt_message      = $_POST['message'];
-                        $mail_subject     = $_POST['subject'];
+                            //Ruta de la plantilla HTML para enviar nuestro mensaje
+                            $template         = "./index.html";
 
-                        sendmail(
-                            $mail_username,
-                            $mail_userpassword,
-                            $mail_setFromEmail,
-                            $mail_setFromName,
-                            $mail_sendtoAddAddressMail,
-                            $txt_message,
-                            $mail_subject,
-                            $template);
-                    }
-                ?>
-                <div class="enviar">
-                    <div class="contact-check">
-                    </div>
-                    <div class="contact-enviar">
-                        <input type="submit" value="Enviar mensaje" name="send">
-                    </div>
-                    <div class="clear"></div>
+                            /* Inicio captura de datos enviados por $_POST para enviar el correo */
+
+                            //Correo electronico que recibira el mensaje
+                            $mail_sendtoAddAddressMail  = $_POST['customer_email'];
+                            $txt_message                = $_POST['message'];
+                            $mail_subject               = $_POST['subject'];
+
+                            sendemail($mail_sendtoAddAddressMail, $mail_subject, $template, $txt_message);
+                        }
+                        ?>
+                            <div class="enviar">
+                                <div class="contact-check">
+                                </div>
+                                <div class="contact-enviar">
+                                    <input type="submit" value="Enviar mensaje" name="send">
+                                </div>
+                                <div class="clear"></div>
+                            </div>
+                    </form>
                 </div>
-            </form>
+            </div>
         </div>
     </div>
-</div>
-</div>
 
-    
+
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+        crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+        crossorigin="anonymous"></script>
 </body>
 
 </html>
--- WEB/old/testmail/sendemail.php
+++ WEB/old/testmail/sendemail.php
@@ -1,38 +1,70 @@
 <?php
+/*
+requiere change configuration from php.ini and install package from composer
 
+openssl.cafile = "C:\xampp\htdocs\Project_New_Reset_AIHD\WEB\vendor\paragonie\certainty\data\cacert-2018-03-07.pem"
+curl.cainfo = "C:\xampp\htdocs\Project_New_Reset_AIHD\WEB\vendor\paragonie\certainty\data\cacert-2018-03-07.pem"
+ */
 use PHPMailer\PHPMailer\PHPMailer;
 
-    function sendemail($mail_username, $mail_userpassword, $mail_setFromEmail, $mail_setFromName, $mail_addAddress, $txt_message, $email_subject, $template){
-        require '../../vendor/phpmailer/phpmailer/src/Exception.php';
-        require '../../vendor/phpmailer/phpmailer/src/PHPMailer.php';
-        require '../../vendor/phpmailer/phpmailer/src/SMTP.php';
+function sendemail($mail_addAddress, $email_subject, $template, $txt_message){
+    require '../../vendor/autoload.php';
 
-        $mail = new PHPMailer();
-        $mail->isSMTP();    // Establecer el correo electronico para utilizar SMTP
-        $mail->Host = 'smtp.gmail.com';     //especificar el servidor de correo a utilizar
-        $mail->SMTPAuth = true;         // Habilitar la autenticacion con SMTP
-        $mail->Username = $mail_username;   // Correo electronico saliente ejemplo: tucorreo@email.com
-        $mail->Password = $mail_userpassword;    // Tu contraseña de gmail saliente
-        $mail->SMTPSecure = 'tls';      // Habilitar encriptacion, 'ssl' es aceptada
-        $mail->Port = 587;      // Puerto TCP para conectarse
-        $mail->setFrom($mail_setFromEmail, $mail_setFromName);      //Introduzca la direccion de la que debe aparecer el correo electronico. Puede utilizar cualquier direccion que el servidor SMTP acepte como valida. El segundo parametro opcional para esta funcion es el nombre que se mostrara com el remitente en lugar de la direccion de correo electronico en si.
-        $mail->addAddress($mail_addAddress);        // Agregar quien recibe el email enviado
-        $message = file_get_contents($template);
-        $message = str_replace('{{first_name}}', $mail_setFromName, $message);
-        $message = str_replace('{{customer_email}}', $mail_setFromEmail, $message);
-        $mail->isHTML(true);  //Establecer el formato de correo electronico en HTML
+    $mail = new PHPMailer();
 
-        $mail->Subject = $email_subject;
-        $mail->msgHTML($message);
-        if (!$mail->send()) {
-            echo '<p style="color:red"> No se pudo enviar el mensaje..';
-            echo 'Error de correo:'. $mail->ErrorInfo;
-            echo '</p>';
-        } else {
-            echo '<p style="color:green">Tu mensaje ha sido enviado!</p>';
-        }
+    // Establecer el correo electronico para utilizar SMTP
+    $mail->isSMTP();
 
+    // Especificar el servidor de correo a utilizar
+    $mail->Host = 'smtp.gmail.com';
 
+    // Habilitar la autenticacion con SMTP
+    $mail->SMTPAuth = true;
+
+    // Habilitar encriptación, 'ssl' es aceptada
+    $mail->SMTPSecure = 'tls';
+
+    // Puerto TCP para conectarse
+    $mail->Port = 587;
+
+    /* Configuracion de variables para enviar el correo */
+    $mail_setFromName       = 'Soporte New Reset A.I.H.D.';
+    $mail_setFromEmail      = 'soportenewreset@gmail.com';
+    $mail_setFromPassword   = 'AG40DA48CT41';
+
+    // Correo electronico saliente ejemplo: tucorreo@email.com
+    $mail->Username = $mail_setFromEmail;
+
+    // Contraseña de correo saliente
+    $mail->Password = $mail_setFromPassword;
+
+    //Introduzca la direccion de la que debe aparecer el correo electronico. Puede utilizar cualquier direccion que el servidor SMTP acepte como valida. El segundo parametro opcional para esta funcion es el nombre que se mostrara com el remitente en lugar de la direccion de correo electronico en si.
+    $mail->setFrom($mail_setFromEmail, $mail_setFromName);
+
+    // Agregar quien recibe el email enviado
+    $mail->addAddress($mail_addAddress);
+
+    $message = $txt_message;
+    $message+= file_get_contents($template);
+    $message = str_replace('{{first_name}}', $mail_setFromName, $message);
+    $message = str_replace('{{customer_email}}', $mail_setFromEmail, $message);
+
+    //Establecer el formato de correo electronico en HTML
+    $mail->isHTML(true);
+
+    $mail->Subject = $email_subject;
+    $mail->msgHTML($message);
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b><br>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    // To load the Spanish version
+    $mail->setLanguage('es', '../../assets/Framework/PHPMailer/');
+
+    if (!$mail->send()) {
+        echo '<p style="color:red"> No se pudo enviar el mensaje..';
+        echo '<br>Error de correo:'. $mail->ErrorInfo;
+        echo '</p>';
+    } else {
+        echo '<p style="color:green">Tu mensaje ha sido enviado!</p>';
     }
-
-?>
+}
