40ee37fb82f97000e6db601e95a251ccca56a534
Unknown <afgarcia0479@misena.edu.co>
2018-06-25 17:40:42 -0500

    Update sendEmail

--- WEB/old/testmail/sendemail.php
+++ WEB/old/testmail/sendemail.php
@@ -44,18 +44,23 @@ function sendemail($mail_addAddress, $email_subject, $template, $txt_message){
     // Agregar quien recibe el email enviado
     $mail->addAddress($mail_addAddress);
 
+    // Activo condificacción utf-8
+    $mail->CharSet = 'UTF-8';
+
     $message = $txt_message;
-    $message+= file_get_contents($template);
-    $message = str_replace('{{first_name}}', $mail_setFromName, $message);
-    $message = str_replace('{{customer_email}}', $mail_setFromEmail, $message);
+    $mail->Subject = 'Restablecer de contraseña NEW RESET A.I.H.D.';
+    $message = 'Puede ingresar al aplicativo en el siguiente enlace solo en la sede Barrio Colombia: https://192.168.3.240:8181 <br> Su Contraseña Es: 8XRXYVH3';
+    //$message+= file_get_contents($template);
+    //$message = str_replace('{{first_name}}', $mail_setFromName, $message);
+    //$message = str_replace('{{customer_email}}', $mail_setFromEmail, $message);
 
     //Establecer el formato de correo electronico en HTML
     $mail->isHTML(true);
 
-    $mail->Subject = $email_subject;
+    //$mail->Subject = $email_subject;
     $mail->msgHTML($message);
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b><br>';
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+    //$mail->Body    = 'This is the HTML message body <b>in bold!</b><br>';
+    //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
     // To load the Spanish version
     $mail->setLanguage('es', '../../assets/Framework/PHPMailer/');
