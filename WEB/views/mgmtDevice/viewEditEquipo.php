d8bcaeb8235a4dc60377f613febe17cf0c031385
Unknown <afgarcia0479@misena.edu.co>
2018-06-26 20:09:38 -0500

    Fix errors viewEditUsers.php

--- WEB/views/mgmtDevice/viewEditEquipo.php
+++ WEB/views/mgmtDevice/viewEditEquipo.php
@@ -17,12 +17,12 @@ if (isset($_GET['id'])) {
     include '../../model/Equipo.php';
     $equipo = new Equipo();
     $equipo->readSingleEquipo($id_equipo);
-
+    
     if ($equipo->getSerial_equipo() == null) {
-
+        
         //Si no encontrado registro de equipo en base de datos, redireccionar -> viewListEquipo.php con parametros
         header("location: ./viewListEquipo.php?info=search&result=no");
-
+        
     } else {
         $id_equipo = $equipo->getId_Equipo();
         $serial_equipo = $equipo->getSerial_equipo();
@@ -57,8 +57,6 @@ if (isset($_GET['id'])) {
 	</div>
 	<hr>
 	<div class="modal-body">
-		<?php
-echo '
 			<form method="post" action="../../controllers/controllerUpdateEquipo.php" class="">
 			<div class= "row">
 			 <div class="col-sm-4">
@@ -67,11 +65,9 @@ echo '
                             <div class="form-group text-center">
                                 <label for="cod_estado_equipo" class="control-label ">Estado de Equipo</label>
                                 <div>
-                                <!-- SELECCIONA ESTADO DE EQUIPO -->';
+                                <!-- SELECCIONA ESTADO DE EQUIPO -->
+                                <?php printBtnRadioCodEstadoEquipo($cod_estado_equipo); ?>
 
-printBtnRadioCodEstadoEquipo($cod_estado_equipo);
-
-echo '
                                 </div>
                             </div>
                       </div>
@@ -79,10 +75,10 @@ echo '
 					 <div class="row">
                         <div class="col-sm-6">
                             <div class="form-group">
-                                <input type="hidden" name="id_equipo" id="id_equipo" class="form-control" value="' . $id_equipo . '">
-                                <input type="hidden" id="serial_equipo" name="serial_equipo" value="' . $serial_equipo . '" class="form-control">
+                                <input type="hidden" name="id_equipo" id="id_equipo" class="form-control" value="<?php echo $id_equipo ?>">
+                                <!-- <input type="hidden" id="serial_equipo" name="serial_equipo" value="<?php //$serial_equipo ?>" class="form-control"> -->
                                 <label for="text_serial_equipo" class="control-label">Serial equipo</label>
-                                <input id="text_serial_equipo" type="text" name="text_serial_equipo" value="' . $serial_equipo . '" class="form-control" disabled>
+                                <input id="text_serial_equipo" type="text" name="serial_equipo" value="<?php echo $serial_equipo ?>" class="form-control" readonly>
                             </div>
                         </div>
 
@@ -90,24 +86,21 @@ echo '
                             <div class="form-group">
                                 <label for="cod_tipo_equipo" class="control-label">Tipo de equipo</label>
                                 <div>
-                                <!-- SELECCIONA TIPO DE EQUIPO -->';
-
-printBtnRadioCodTipoEquipo($cod_tipo_equipo);
-
-echo '
-                          </div>
+                                <!-- SELECCIONA TIPO DE EQUIPO -->
+                                <?php printBtnRadioCodTipoEquipo($cod_tipo_equipo); ?>
+                                </div>
                             </div>
                         </div>
-                    </div>'; ?>
+                    </div>
                     <hr>
                     <div class="row">
 
                         <div class="col-lg-5 col-md-5 col-sm-5 col-xs-12 pull-right">
-                            <button id="enviar" name="btnClickedEquipo" value="sendUpdate" type="submit" class="btn btn-block btn-success pmd-z-depth-3 pmd-ripple-effect" onsubmit="return checkPass()">Actualizar datos</button>
+                            <button id="enviar" name="btnClickedUser" value="sendUpdate" type="submit" class="btn btn-block btn-success pmd-z-depth-3 pmd-ripple-effect">Actualizar datos</button>
                         </div>
                         <div class="col-lg-2 col-md-2 col-sm-2"></div>
                         <div class="col-lg-5 col-md-5 col-sm-5 col-xs-12 pull-right">
-                            <a  onClick="goToListEquipo()">
+                            <a  onClick="goToListDevices()">
                                 <button id="cancelar" name="cancelar" type="button" class="btn btn-block btn-danger pmd-z-depth-3 pmd-ripple-effect">Cancelar</button>
                             </a>
                         </div>
